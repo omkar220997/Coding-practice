152ea650ee7c8ded50b908d9cc05b8ed2c4c60e2
omkar kadam <omkar.kadam@klingelnberg.com>
2022-12-12 16:58:36 +0530

    Class file programming with all operatrion

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -10,6 +10,15 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter first digit : ");
+            string astr = Console.ReadLine();
+            double a = Convert.ToDouble(astr);
+            Console.WriteLine("Enter second digit : ");
+            string bstr = Console.ReadLine();
+            double b = Convert.ToDouble(astr);
+
+            Console.ReadLine();
+
         }
     }
 }
---
+++ Calculator/operation.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class operation
+    {
+        public double addition(double a, double b)
+        {
+            double sum=a+b;
+            Console.WriteLine($"the addition of {a} and {b} is : " + sum);
+            return sum;
+        }
+        public double substraction(double a, double b)
+        {
+            double sub=a-b;
+            Console.WriteLine($"the substraction of {a} and {b} is : " + sub);
+            return sub;
+        }
+        public double multiplication(double a, double b)
+        {
+            double mul=a*b;
+            Console.WriteLine($"the multiplication of {a} and {b} is : " + mul);
+            return mul;
+        }
+        public double division(double a, double b)
+        {
+            double div=a/b;
+            Console.WriteLine($"the division of {a} and {b} is : " + div);
+            return div;
+        }
+        public double  modulus(double a, double b)
+        {
+            double mod=a%b;
+            Console.WriteLine($"the modulus of {a} and {b} is : " + mod);
+            return mod;
+        }
+        public double  square(double a)
+        {
+            double sqr=a*a;
+            Console.WriteLine($"the square of {a} is : " + sqr);
+            return sqr;
+        }
+        public double  sqruareRoot (double a)
+        {
+            double sqrt=Math.Sqrt(a);
+            Console.WriteLine($"the square root of {a}  is : " + sqrt);
+            return sqrt;
+        }
+    }
+}
